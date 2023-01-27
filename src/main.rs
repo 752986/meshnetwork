2befbb58f1447ac16103f19e0ad7bfdca9a81205
Dec <112513827+752986@users.noreply.github.com>
2023-01-27 14:51:01 -0700

    Start implementing protocol
    Co-authored-by: CriticalHex <CriticalHex@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{
+    collections::HashMap,
     io::{read_to_string, BufRead, BufReader, Read, Write},
     net::{Shutdown, TcpListener, TcpStream},
 };
 
 fn main() {
-    let listener: TcpListener = TcpListener::bind("0.0.0.0:42069").expect("unable to bind listener");
+    let listener: TcpListener =
+        TcpListener::bind("0.0.0.0:42069").expect("unable to bind listener");
 
     loop {
         match listener.accept() {
@@ -23,6 +25,62 @@ fn main() {
             }
             Err(ip) => {
                 println!("unable to connect to client at {ip}");
+            }
+        }
+    }
+}
+
+type Id = u32;
+
+struct Peer {
+    stream: TcpStream,
+}
+
+impl Peer {
+    fn send(&mut self, data: PacketData) {
+        // turn the given packetdata into bytes and send it to the peer
+        todo!()
+        // self.stream.write_all(data);
+    }
+}
+
+struct Packet<'a> {
+    from: Peer,
+    data: PacketData<'a>,
+}
+
+enum PacketData<'a> {
+    /// A notification that a new piece of data is available
+    Available { id: Id },
+    /// A request for data with the given id
+    Request { id: Id },
+    /// A piece of data
+    Data { id: Id, data: &'a [u8] },
+}
+
+struct Server {
+    seen_data: HashMap<Id, Vec<u8>>,
+    connections: Vec<Peer>,
+}
+
+impl Server {
+    fn handleIncomingPacket(&mut self, mut packet: Packet) {
+        match packet.data {
+            PacketData::Request { id } => {
+                if self.seen_data.contains_key(&id) {
+                    packet.from.send(PacketData::Data {
+                        id,
+                        data: &self.seen_data[&id],
+                    })
+                }
+            }
+            PacketData::Available { id } => {
+                if !(self.seen_data.contains_key(&id)) {
+                    packet.from.send(PacketData::Request { id })
+                }
+            }
+            PacketData::Data { id, data } => {
+                self.seen_data.insert(id, data.to_vec());
             }
         }
     }
